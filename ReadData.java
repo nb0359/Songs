f8e2e0718ccf0d251c4085e97c6231c7a30ed86a
nb0359 <nb0359@pleasantonusd.net>
2025-10-13 11:46:00 -0700

    uploading files

---
+++ ReadData.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+
+public class ReadData{
+    public void getData(){
+        try{
+            int count = 1;
+            int longestName = 0;
+            
+            Scanner in = new Scanner(new File("songsANSI.csv"));
+
+            while (in.hasNext()) {
+                String line = in.nextLine();
+                System.out.println(count + ": " + line.split(",")[0]);
+                count++;
+                int length = line.length();
+                if (line.length()>longestName){
+                    length = longestName;
+                    
+
+                }
+                else{
+                    longestName = longestName;
+                }
+                System.out.println(longestName);
+        }
+
+        in.close();
+
+        }
+        catch(IOException e){
+            System.out.println("Error in file reading");
+            
+        }
+
+        
+
+    
+    }
+}
