56d7b881e3eda8918f3eaac5e884cd750c046088
nb0359 <nb0359@pleasantonusd.net>
2025-10-15 11:45:06 -0700

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,9 +1,12 @@
 public class Driver {
     public static void main(String [] args){
         ReadData x = new ReadData();
+        Songs [] mySongs = r.read();
+        System.out.println();
 
         x.getData();
 
         
     }
 }
+
