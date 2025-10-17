3b476f62f1eae432c5c4322a2bda0523a1710028
nb0359 <nb0359@pleasantonusd.net>
2025-10-16 21:24:23 -0700

    clean up

--- Song.java
+++ Song.java
@@ -70,9 +70,5 @@ public class Song {
         return (releaseDate / 10) * 10;
     }
     
-    @Override
-    public String toString() {
-        return String.format("%s - %s (%d) [%s]", 
-            artistName, trackName, releaseDate, genre);
-    }
+    
 }
